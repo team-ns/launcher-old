80761257d6ce212144a2117d31143bdbe78cce0b
Jeb <osnovchig@gmail.com>
2020-03-04 20:46:52 +0300

    Add auth response

--- src/message.rs
+++ src/message.rs
@@ -1,20 +1,36 @@
-use serde::{Deserialize, Serialize};
 use actix::prelude::Message as Msg;
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Msg)]
 #[rtype(result = "()")]
-pub enum Message {
+pub enum ClientMessage {
     Auth(AuthMessage),
     Profiles(ProfilesMessage)
 }
+
+#[derive(Deserialize, Serialize, Msg)]
+#[rtype(result = "()")]
+pub enum ServerMessage {
+    Auth(AuthResponse),
+}
+
 #[derive(Deserialize, Serialize, Msg)]
 #[rtype(result = "()")]
 pub struct AuthMessage {
     pub login: String,
     pub password: String,
 }
+
 #[derive(Deserialize, Serialize, Msg)]
 #[rtype(result = "()")]
 pub struct ProfilesMessage {
 
+}
+
+#[derive(Deserialize, Serialize, Msg)]
+#[serde(rename_all = "camelCase")]
+#[rtype(result = "()")]
+pub struct AuthResponse {
+    pub uuid: String,
+    pub access_token: String,
 }
