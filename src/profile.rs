273a119d959de23edd9e1eadc3e155f550cd9890
Artem Belz <kyzacrab@gmail.com>
2020-03-04 16:42:43 +0200

    Fix field visibility

--- src/profile.rs
+++ src/profile.rs
@@ -3,13 +3,13 @@ use serde::{Serialize, Deserialize};
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Profile {
-    name: String,
-    version: String,
-    libraries: Vec<String>,
-    class_path: Vec<String>,
-    main_class: String,
-    jvm_args: Vec<String>,
-    client_args: Vec<String>,
-    assets: String,
-    assets_dir: String,
+    pub name: String,
+    pub version: String,
+    pub libraries: Vec<String>,
+    pub class_path: Vec<String>,
+    pub main_class: String,
+    pub jvm_args: Vec<String>,
+    pub client_args: Vec<String>,
+    pub assets: String,
+    pub assets_dir: String,
 }
