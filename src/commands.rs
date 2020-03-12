e20136de3278051c2e18929393a56de53e06b561
Jeb <osnovchig@gmail.com>
2020-03-12 18:15:08 +0300

    Fix commands

--- src/commands.rs
+++ src/commands.rs
@@ -1,16 +1,20 @@
-use shrust::{ShellIO, Shell};
-use crate::config::Config;
 use std::io::Write;
+use std::thread;
+
+use log::info;
+use shrust::{Shell, ShellIO};
+
+use crate::config::Config;
 
 pub fn start(config: Config) {
     let mut shell = Shell::new(config);
     register_commands(&mut shell);
-    shell.run_loop(&mut ShellIO::default());
+    thread::spawn( move || shell.run_loop(&mut ShellIO::default()));
 }
 
 fn register_commands(shell: &mut Shell<Config>) {
     shell.new_command_noargs("build", "Build launcher", |io, v| {
-        writeln!(io, "Not working");
+        info!("Not working");
         Ok(())
     });
 }
