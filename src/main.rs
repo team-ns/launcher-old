c01f0f890adb97c5c5aa1ccd7115b1646c140ab8
Artem Belz <kyzacrab@gmail.com>
2020-03-19 12:47:14 +0200

    Use relm for runtime

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use launcher_api::config::Configurable;
 use config::Config;
+use relm::Widget;
 
 mod game;
 mod security;
@@ -8,7 +9,7 @@ mod client;
 mod runtime;
 
 fn main() {
-    runtime::start();
+    runtime::Runtime::run(()).unwrap();
     let config = Config::get_config(
         dirs::config_dir().unwrap()
             .join("nsl")
--- src/runtime.rs
+++ src/runtime.rs
@@ -1,50 +1,77 @@
-use gio::prelude::*;
 use gtk::prelude::*;
-use gtk::{ApplicationWindow, Builder, Button, MessageDialog, CssProvider};
+use gtk::{ApplicationWindow, Builder, Button};
+use relm_derive::Msg;
+use relm::{Relm, Update, Widget};
 
-pub fn start() {
-    let application = gtk::Application::new(
-        Some("nslauncher"),
-        Default::default(),
-    ).expect("Initialization failed...");
+use crate::client::ClientHandler;
+use crate::client;
 
-    let provider = gtk::CssProvider::new();
-    provider
-        .load_from_data(include_str!("../runtime/style.css").as_ref())
-        .expect("Failed to load CSS");
+mod resources;
 
-    gtk::StyleContext::add_provider_for_screen(
-        &gdk::Screen::get_default().expect("Error initializing gtk css provider."),
-        &provider,
-        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
-    );
-
-    application.connect_activate(|app| {
-        create_ui(app);
-    });
-
-    application.run(&[]);
+#[derive(Msg)]
+pub enum Msg {
+    Login,
 }
 
-fn create_ui(application: &gtk::Application) {
-    let glade_src = include_str!("../runtime/main.glade");
-    let builder = Builder::new_from_string(glade_src);
+pub struct Model {
+    client_handler: ClientHandler,
+}
 
-    builder.connect_signals(move |_, handler_name| {
-        if handler_name == "login" {
-            Box::new(move |_| {
-                login();
-                None
-            })
-        } else {
-            panic!("Unknown handler name {}", handler_name)
+pub struct Runtime {
+    model: Model,
+    window: ApplicationWindow,
+}
+
+
+impl Update for Runtime {
+    type Model = Model;
+    type ModelParam = ();
+    type Msg = Msg;
+
+    fn model(_: &Relm<Self>, _: ()) -> Model {
+        Model {
+            client_handler: client::ClientHandler::new("ws://localhost:9090/api/"),
         }
-    });
-    let window: ApplicationWindow = builder.get_object("window").unwrap();
-    window.set_application(Some(application));
-    window.show_all();
+    }
+
+    fn update(&mut self, event: Msg) {
+        match event {
+            Msg::Login =>  self.model.client_handler.auth("Test", "test"),
+        }
+    }
 }
 
-fn login() {
-    println!("login");
+impl Widget for Runtime {
+    type Root = ApplicationWindow;
+
+    fn root(&self) -> Self::Root {
+        self.window.clone()
+    }
+
+    fn view(relm: &Relm<Self>, model: Self::Model) -> Self {
+        let provider = gtk::CssProvider::new();
+        provider
+            .load_from_data(include_str!("../runtime/style.css").as_ref())
+            .expect("Failed to load CSS");
+
+        gtk::StyleContext::add_provider_for_screen(
+            &gdk::Screen::get_default().expect("Error initializing gtk css provider."),
+            &provider,
+            gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+        );
+        let glade_src = include_str!("../runtime/main.glade");
+
+        let builder = Builder::new_from_string(glade_src);
+        let button: Button = builder.get_object("login").unwrap();
+        let window: ApplicationWindow = builder.get_object("window").unwrap();
+
+        relm::connect!(relm, button, connect_clicked(_), Msg::Login);
+
+        window.show_all();
+
+        Runtime {
+            model,
+            window,
+        }
+    }
 }
