d368a412acb7946db25f021b894eaddca53ca6d5
Jeb <osnovchig@gmail.com>
2020-03-30 08:31:22 +0300

    New websocket and remove gtk

--- src/client.rs
+++ src/client.rs
@@ -1,38 +1,114 @@
-use tokio_tungstenite::{connect_async, WebSocketStream};
-use futures_util::{StreamExt, SinkExt};
-use futures_util::stream::SplitSink;
-use tungstenite::protocol::Message::Text;
-use tungstenite::protocol::Message;
-use tokio::net::TcpStream;
-use tokio::runtime::Runtime;
-use launcher_api::message::{ClientMessage, AuthMessage, ServerMessage};
+use std::sync::mpsc::{channel, Sender};
+use std::sync::mpsc::Receiver;
+use std::thread;
 
-use crate::security::SecurityManager;
+use launcher_api::message::{AuthMessage, ClientMessage, ServerMessage};
+use launcher_api::message::Error;
+use launcher_api::message::ServerMessage::{Auth, Error as OtherError};
+use openssl::ssl::{SslConnector, SslMethod, SslStream};
+use ws::{ErrorKind, Handler, Handshake, Request, WebSocket};
+use ws::Message;
+use ws::util::TcpStream;
+
 use crate::security;
+use crate::security::SecurityManager;
+use url::Url;
 
-pub struct ClientHandler {
-    writer: SplitSink<WebSocketStream<TcpStream>, Message>,
-    runtime: Runtime,
+pub struct WebSocketClient {
+    out: ws::Sender,
+    recv: Receiver<ServerMessage>,
     security: SecurityManager,
 }
 
-impl ClientHandler {
+pub struct Client {
+    out: ws::Sender,
+    sender: Sender<ServerMessage>
+}
+
+impl Handler for Client {
+
+    fn on_message(&mut self, msg: Message) -> ws::Result<()> {
+        match msg {
+            Message::Text(t) => {
+                if let Ok(message) = serde_json::from_str::<ServerMessage>(&t) {
+                    self.sender.send(message);
+                } else {
+                    self.sender.send(ServerMessage::Error(Error { msg: "work".to_string() }));
+                }
+            }
+            Message::Binary(_) => {}
+        }
+        Ok(())
+    }
+
+    fn build_request(&mut self, url: &Url) -> ws::Result<Request> {
+        let mut req = Request::from_url(url)?;
+        req.headers_mut()
+            .push(("Origin".into(), get_origin(url).into()));
+
+
+        if true {
+            eprintln!("WebSocket upgrade request");
+            eprintln!("---");
+            eprintln!("{}", req);
+        }
+
+        Ok(req)
+    }
+
+
+
+
+    /* fn upgrade_ssl_client(
+         &mut self,
+         sock: TcpStream,
+         _: &url::Url,
+     ) -> ws::Result<SslStream<TcpStream>> {
+         let mut builder = SslConnector::builder(SslMethod::tls()).map_err(|e| {
+             ws::Error::new(
+                 ws::ErrorKind::Internal,
+                 format!("Failed to upgrade client to SSL: {}", e),
+             )
+         })?;
+         builder.set_verify(SslVerifyMode::empty());
+
+         let connector = builder.build();
+         connector
+             .configure()
+             .unwrap()
+             .use_server_name_indication(false)
+             .verify_hostname(false)
+             .connect("", sock)
+             .map_err(From::from)
+     }*/
+}
+
+
+impl WebSocketClient {
     pub fn new(address: &str) -> Self {
-        let mut rt = Runtime::new().unwrap();
-        let socket = rt.block_on(connect_async(address)).unwrap().0;
+        let ( s, r) = channel::<ServerMessage>();
+        let mut ws = WebSocket::new(move |out|
+            Client {
+                sender: s.clone(),
+                out
+            }
+        ).unwrap();
+        let parsed: url::Url = Url::parse(&address.to_string()).unwrap();
+        ws.connect(parsed);
+        let sender = ws.broadcaster();
 
-        let (write, read) = socket.split();
-        rt.spawn(async {
-            use tokio::runtime::Handle;
-
-            let handle = Handle::current();
-            handle.spawn(
-                read.for_each(|message| async {
-                handle_message(message.unwrap());
-            }));
-        });
-
-        ClientHandler { writer: write, runtime: rt, security: security::get_manager()}
+        thread::Builder::new()
+            .name("websocket_handler".to_owned())
+            .spawn(move || {
+                // This blocks the thread
+                ws.run();
+            })
+            .expect("Failed to start WebSocket thread");
+        WebSocketClient {
+            security: security::get_manager(),
+            recv: r,
+            out: sender.clone(),
+        }
     }
 
 
@@ -40,27 +116,38 @@ impl ClientHandler {
         let message = ClientMessage::Auth(
             AuthMessage {
                 login: String::from(login),
-                password: self.security.encrypt(password)
+                password: self.security.encrypt(password),
             }
         );
-        self.send_message(message);
+        match self.send_sync(message) {
+            Auth(auth) => {
+                println!("UUID - {} : TOKEN - {}", auth.uuid, auth.access_token);
+            }
+            OtherError(e) => { println!("Error: {}", e.msg) }
+        };
     }
 
-    fn send_message(&mut self, message: ClientMessage) {
-        self.runtime.block_on(
-            self.writer.send(Message::Text(serde_json::to_string(&message).unwrap()))
-        ).unwrap();
+
+    fn send_sync(&mut self, msg: ClientMessage) -> ServerMessage {
+        self.out.send(Message::text(serde_json::to_string(&msg).unwrap()));
+        match self.recv.recv() {
+            Ok(message) => {
+                message
+            }
+            Err(e) => {
+                ServerMessage::Error(Error { msg: "what".to_string() })
+            }
+        }
+
     }
 }
 
-fn handle_message(message: Message) {
-    if message.is_text() {
-        let sever_msg: ServerMessage = serde_json::from_str(&message.into_text().unwrap()).unwrap();
-        match sever_msg {
-            ServerMessage::Auth(auth) => {
-                println!("UUID - {} : TOKEN - {}", auth.uuid, auth.access_token);
-            }
-            _ => {}
-        }
-    }
+fn get_origin(url: &Url) -> String {
+    let scheme = if url.scheme() == "wss" {
+        "https"
+    } else {
+        "http"
+    };
+
+    format!("{}://{}", scheme, url.host_str().unwrap_or(""))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use launcher_api::config::Configurable;
 use config::Config;
-use relm::Widget;
+use crate::client::WebSocketClient;
+use std::thread;
+use std::time::Duration;
 
 mod game;
 mod security;
@@ -9,14 +11,17 @@ mod client;
 mod runtime;
 
 fn main() {
-    runtime::Runtime::run(()).unwrap();
-    let config = Config::get_config(
+    env_logger::init();
+    let mut socket = WebSocketClient::new("ws://127.0.0.1:8080/api/");
+    thread::sleep(Duration::from_secs(5));
+    socket.auth("Test", "test");
+   /* let config = Config::get_config(
         dirs::config_dir().unwrap()
             .join("nsl")
             .join("config.json")
             .as_path()
     ).unwrap();
     let client = game::Client{name: String::from("test")};
-    game::Client::start(&client, &config.game_dir);
+    game::Client::start(&client, &config.game_dir);*/
 }
 
