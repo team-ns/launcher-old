fe65f834b8387dd6aaa0b3da6a78a16d42a0bb9d
RussianJeb <russianjeb@protonmail.com>
2020-05-31 12:21:53 +0300

    Remove actix message

--- src/message.rs
+++ src/message.rs
@@ -1,43 +1,36 @@
-use actix::prelude::Message as Msg;
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize, Msg)]
-#[rtype(result = "()")]
+#[derive(Deserialize, Serialize)]
 pub enum ClientMessage {
     Auth(AuthMessage),
     Profiles(ProfilesMessage)
 }
 
-#[derive(Deserialize, Serialize, Msg)]
-#[rtype(result = "()")]
+#[derive(Deserialize, Serialize)]
 pub enum ServerMessage {
     Auth(AuthResponse),
     Error(Error)
 }
 
-#[derive(Deserialize, Serialize, Msg)]
-#[rtype(result = "()")]
+#[derive(Deserialize, Serialize)]
 pub struct AuthMessage {
     pub login: String,
     pub password: String,
 }
 
-#[derive(Deserialize, Serialize, Msg)]
-#[rtype(result = "()")]
+#[derive(Deserialize, Serialize)]
 pub struct ProfilesMessage {
 
 }
 
-#[derive(Deserialize, Serialize, Msg)]
+#[derive(Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
-#[rtype(result = "()")]
 pub struct AuthResponse {
     pub uuid: String,
     pub access_token: String,
 }
 
-#[derive(Deserialize, Serialize, Msg)]
-#[rtype(result = "()")]
+#[derive(Deserialize, Serialize)]
 pub struct Error {
     pub msg: String,
 }
