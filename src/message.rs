6aa9c6012aee761bfb87cce9c6b95bd12a9a5a3c
RussianJeb <russianjeb@protonmail.com>
2020-05-31 12:34:25 +0300

    Revert "Remove actix message"
    This reverts commit 52063227

--- src/message.rs
+++ src/message.rs
@@ -1,57 +1,48 @@
-use actix::prelude::Message as Msg;
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize, Msg)]
-#[rtype(result = "()")]
+#[derive(Deserialize, Serialize)]
 pub enum ClientMessage {
     Auth(AuthMessage),
     Profiles(ProfilesMessage),
     ProfileResources(ProfileResourcesMessage)
 }
 
-#[derive(Deserialize, Serialize, Msg)]
-#[rtype(result = "()")]
+#[derive(Deserialize, Serialize)]
 pub enum ServerMessage {
     Auth(AuthResponse),
     ProfileResources(ProfileResourcesResponse),
     Error(Error)
 }
 
-#[derive(Deserialize, Serialize, Msg)]
-#[rtype(result = "()")]
+#[derive(Deserialize, Serialize)]
 pub struct AuthMessage {
     pub login: String,
     pub password: String,
 }
 
-#[derive(Deserialize, Serialize, Msg)]
-#[rtype(result = "()")]
+#[derive(Deserialize, Serialize)]
 pub struct ProfileResourcesMessage {
     pub profile: String
 }
 
-#[derive(Deserialize, Serialize, Msg)]
-#[rtype(result = "()")]
+#[derive(Deserialize, Serialize)]
 pub struct ProfilesMessage {
 
 }
 
-#[derive(Deserialize, Serialize, Msg)]
-#[rtype(result = "()")]
+#[derive(Deserialize, Serialize)]
 pub struct ProfileResourcesResponse {
     pub list: Vec<String>
 }
 
-#[derive(Deserialize, Serialize, Msg)]
+#[derive(Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
-#[rtype(result = "()")]
 pub struct AuthResponse {
     pub uuid: String,
     pub access_token: String,
 }
 
-#[derive(Deserialize, Serialize, Msg)]
-#[rtype(result = "()")]
+#[derive(Deserialize, Serialize)]
 pub struct Error {
     pub msg: String,
 }
