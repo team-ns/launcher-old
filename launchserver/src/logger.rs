f428df19fa28b45c47f1aa1860e6fa4bfe4b40b3
RussianJeb <russianjeb@protonmail.com>
2020-10-29 21:08:35 +0300

    feat(server): write logger configuration for release build

--- launchserver/src/logger.rs
+++ launchserver/src/logger.rs
@@ -1,5 +1,5 @@
 use log::LevelFilter;
-use log4rs::append::console::{ConsoleAppender, Target};
+use log4rs::append::console::ConsoleAppender;
 use log4rs::append::file::FileAppender;
 use log4rs::config::{Appender, Config, Logger, Root};
 use log4rs::encode::pattern::PatternEncoder;
@@ -35,4 +35,31 @@ pub fn configure() {
 }
 
 #[cfg(not(debug_assertions))]
-pub fn configure() {}
+pub fn configure() {
+    let stdout = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(
+            "{d(%Y-%m-%d %H:%M:%S)} {h([{l} {M}])}: {m}{n}",
+        )))
+        .build();
+
+    let logfile = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(
+            "{d(%Y-%m-%d %H:%M:%S)} {h([{l} {M}])}: {m}{n}",
+        )))
+        .append(false)
+        .build("launcher.log")
+        .unwrap();
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .build(
+            Root::builder()
+                .appender("logfile")
+                .appender("stdout")
+                .build(LevelFilter::Info),
+        )
+        .unwrap();
+
+    log4rs::init_config(config).expect("Can't init logger");
+}
