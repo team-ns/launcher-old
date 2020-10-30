28321ec8eea5dfbad25cc2186d795ecb3ab64985
RussianJeb <russianjeb@protonmail.com>
2020-10-30 07:27:53 +0300

    refactor(server): remove duplication code and use expect instead unwrap

--- launchserver/src/logger.rs
+++ launchserver/src/logger.rs
@@ -4,7 +4,6 @@ use log4rs::append::file::FileAppender;
 use log4rs::config::{Appender, Config, Logger, Root};
 use log4rs::encode::pattern::PatternEncoder;
 
-#[cfg(debug_assertions)]
 pub fn configure() {
     let stdout = ConsoleAppender::builder()
         .encoder(Box::new(PatternEncoder::new(
@@ -18,48 +17,20 @@ pub fn configure() {
         )))
         .append(false)
         .build("launcher.log")
-        .unwrap();
+        .expect("Can't build logfile manager");
 
-    let config = Config::builder()
+    let root_builder = Root::builder().appender("logfile").appender("stdout");
+
+    let mut config_builder = Config::builder()
         .appender(Appender::builder().build("stdout", Box::new(stdout)))
-        .appender(Appender::builder().build("logfile", Box::new(logfile)))
-        .logger(Logger::builder().build("rustyline", LevelFilter::Info))
-        .build(
-            Root::builder()
-                .appender("logfile")
-                .appender("stdout")
-                .build(LevelFilter::Debug),
-        )
-        .unwrap();
-    log4rs::init_config(config).expect("Can't init logger");
-}
+        .appender(Appender::builder().build("logfile", Box::new(logfile)));
 
-#[cfg(not(debug_assertions))]
-pub fn configure() {
-    let stdout = ConsoleAppender::builder()
-        .encoder(Box::new(PatternEncoder::new(
-            "{d(%Y-%m-%d %H:%M:%S)} {h([{l} {M}])}: {m}{n}",
-        )))
-        .build();
-
-    let logfile = FileAppender::builder()
-        .encoder(Box::new(PatternEncoder::new(
-            "{d(%Y-%m-%d %H:%M:%S)} {h([{l} {M}])}: {m}{n}",
-        )))
-        .append(false)
-        .build("launcher.log")
-        .unwrap();
-
-    let config = Config::builder()
-        .appender(Appender::builder().build("stdout", Box::new(stdout)))
-        .appender(Appender::builder().build("logfile", Box::new(logfile)))
-        .build(
-            Root::builder()
-                .appender("logfile")
-                .appender("stdout")
-                .build(LevelFilter::Info),
-        )
-        .unwrap();
-
-    log4rs::init_config(config).expect("Can't init logger");
+    let mut config = if cfg!(debug_assertions) {
+        config_builder
+            .logger(Logger::builder().build("rustyline", LevelFilter::Info))
+            .build(root_builder.build(LevelFilter::Debug))
+    } else {
+        config_builder.build(root_builder.build(LevelFilter::Info))
+    };
+    log4rs::init_config(config.expect("Can't build logger")).expect("Can't init logger");
 }
