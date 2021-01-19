91dd3261cac5a3f37392be394e9c71ccfdaf1c60
Artem Belz <kyzacrab@gmail.com>
2021-01-19 16:27:01 +0200

    fix(api): change config generation

--- launcherapi/src/config.rs
+++ launcherapi/src/config.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 use std::fs;
@@ -5,20 +6,23 @@ use std::fs::OpenOptions;
 use std::path::Path;
 
 pub trait Configurable: Default + Serialize + DeserializeOwned {
-    fn get_config(config_path: &Path) -> std::io::Result<Self> {
-        fs::create_dir_all(config_path.parent().unwrap())?;
-        let config_file = OpenOptions::new()
-            .write(true)
-            .create(true)
-            .read(true)
-            .open(config_path)?;
-        match serde_json::from_reader(&config_file) {
-            Ok(config) => Ok(config),
-            Err(_e) => {
-                let config = Self::default();
-                serde_json::to_writer_pretty(&config_file, &config)?;
-                Ok(config)
-            }
+    fn get_config(config_path: &Path) -> Result<Self> {
+        if let Some(path) = config_path.parent() {
+            fs::create_dir_all(path)?;
         }
+
+        if !config_path.exists() {
+            let file = OpenOptions::new()
+                .create(true)
+                .write(true)
+                .open(config_path)?;
+
+            let config = Self::default();
+            serde_json::to_writer_pretty(&file, &config)?;
+            return Ok(config);
+        }
+
+        let config_file = OpenOptions::new().read(true).open(config_path)?;
+        serde_json::from_reader(&config_file).map_err(|error| anyhow::anyhow!(error))
     }
 }
