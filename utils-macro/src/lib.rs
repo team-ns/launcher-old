780db0e982c9f38e038bd536492efd8f41fd1783
Artem Belz <kyzacrab@gmail.com>
2021-04-28 23:17:19 +0300

    feat(macro): add macro for rehash

--- utils-macro/src/lib.rs
+++ utils-macro/src/lib.rs
@@ -1,10 +1,12 @@
-mod args;
-
+use args::{Args, Command};
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, AttributeArgs, ItemFn};
+use syn::{Expr, Ident};
 
-use args::{Args, Command};
+use crate::args::Resource;
+
+mod args;
 
 #[proc_macro_attribute]
 pub fn command(attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -47,13 +49,33 @@ pub fn command(attr: TokenStream, item: TokenStream) -> TokenStream {
 
 #[proc_macro]
 pub fn register_commands(item: TokenStream) -> TokenStream {
-    let args = parse_macro_input!(item as Args);
+    let args = parse_macro_input!(item as Args<Ident>);
     let mut stream = TokenStream::new();
     for ident in args.vars {
         let varname = Command::get_varname(&ident);
         stream.extend(TokenStream::from(quote! {
             helper.new_command(&#varname);
         }));
+    }
+    stream
+}
+
+#[proc_macro]
+pub fn hash(item: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(item as Args<Expr>);
+    let mut stream = TokenStream::new();
+    let hash_args = args.vars.get(0).expect("Failed to get args value!");
+    for expr in args.vars.iter().skip(1) {
+        if let Expr::MethodCall(expr) = expr {
+            let resource_name = Resource::get_name(&expr.method);
+            stream.extend(TokenStream::from(quote! {
+                let resource_name = #resource_name;
+                if #hash_args.is_empty() || #hash_args.contains(&resource_name) {
+                #expr.await;
+                info!("Successfully rehash {}!", resource_name);
+            }
+            }));
+        }
     }
     stream
 }
