41656a70285c46ad281f25871e7fa97f2d29d16f
RussianJeb <russianjeb@protonmail.com>
2022-01-24 16:12:44 +0300

    feat(client): routing in webview using encrypted runtime

--- launcher/src/runtime.rs
+++ launcher/src/runtime.rs
@@ -5,12 +5,10 @@ use anyhow::Result;
 use once_cell::sync::OnceCell;
 use tokio::runtime::Runtime;
 use tokio::sync::Mutex;
-use wry::application::event::{Event, WindowEvent};
-use wry::application::event_loop::ControlFlow;
 
 use crate::client::Client;
 use crate::runtime::arg::{Invoke, InvokeMessage, InvokePayload, InvokeResolver};
-use crate::runtime::webview::{create_webview, create_window, EventProxy, WebviewEvent};
+use crate::runtime::webview::EventProxy;
 
 mod arg;
 mod messages;
@@ -24,71 +22,10 @@ pub static PLAYING: OnceCell<()> = OnceCell::new();
 static RUNTIME: OnceCell<Runtime> = OnceCell::new();
 
 pub fn run() {
-    let (window, event_loop) = create_window().expect("Can't create window");
-    let proxy = event_loop.create_proxy();
-    let (webview, event_loop) = match create_webview(window, proxy) {
-        Ok(w) => (w, event_loop),
-        Err(e) => {
-            #[cfg(target_os = "windows")]
-            {
-                use wry::Error as WVError;
-                match e.downcast::<WVError>() {
-                    Err(e) => {
-                        panic!("{}", e)
-                    }
-                    Ok(WVError::WebView2Error(e)) => {
-                        if e.hresult() == -2147024894 {
-                            webview::download_webview2();
-                            drop(event_loop);
-                            let (window, event_loop) =
-                                create_window().expect("Can't create window");
-                            let proxy = event_loop.create_proxy();
-                            let w = create_webview(window, proxy).expect("Can't create webview");
-                            (w, event_loop)
-                        } else {
-                            panic!("{:?}", e)
-                        }
-                    }
-                    Ok(e) => panic!("{:?}", e),
-                }
-            }
-            #[cfg(not(target_os = "windows"))]
-            {
-                panic!("{}", e)
-            }
-        }
-    };
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Poll;
-
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            Event::UserEvent(event) => match event {
-                WebviewEvent::DispatchScript(s) => {
-                    webview.evaluate_script(&s).expect("Can't invoke js")
-                }
-                WebviewEvent::HideWindow => webview.window().set_visible(false),
-                WebviewEvent::Exit => {
-                    *control_flow = ControlFlow::Exit;
-                }
-                WebviewEvent::ShowWindow => webview.window().set_visible(true),
-                WebviewEvent::Emit(event, payload) => webview
-                    .evaluate_script(&format!(
-                        "window['{}']({{event: {}, payload: {}}})",
-                        "b03ebfab-8145-44ac-a4b6-d800ddfa3bba",
-                        serde_json::to_string(&event).expect("Can't serialize event name"),
-                        payload
-                    ))
-                    .expect("Can't emit event"),
-            },
-            _ => {
-                let _ = webview.resize();
-            }
-        }
-    });
+    if cfg!(target_os = "windows") && !webview::has_webview() {
+        webview::install_webview2()
+    }
+    webview::launch().expect("Can't run launcher webview");
 }
 
 fn handle(proxy: EventProxy, command: String, payload: InvokePayload) -> Result<()> {
--- launcher/src/runtime/webview.rs
+++ launcher/src/runtime/webview.rs
@@ -1,12 +1,13 @@
-#[cfg(target_os = "windows")]
-use std::path::Path;
+use std::process;
 
 use anyhow::Result;
+
 use serde_json::Value;
 use wry::application::dpi::PhysicalSize;
-use wry::application::event_loop::{EventLoop, EventLoopProxy};
-use wry::application::window::Window;
-use wry::webview::WebView;
+use wry::application::event::{Event, WindowEvent};
+use wry::application::event_loop::{ControlFlow, EventLoop, EventLoopProxy};
+
+use wry::http::ResponseBuilder;
 
 use crate::config::BUNDLE;
 use crate::runtime::arg::InvokePayload;
@@ -23,10 +24,12 @@ pub enum WebviewEvent {
     Exit,
 }
 
-pub fn create_window() -> Result<(Window, EventLoop<WebviewEvent>)> {
+pub fn launch() -> Result<()> {
     use wry::application::window::WindowBuilder;
+    use wry::webview::WebViewBuilder;
 
     let event_loop = EventLoop::<WebviewEvent>::with_user_event();
+    let proxy = event_loop.create_proxy();
 
     let window = WindowBuilder::new()
         .with_decorations(!BUNDLE.window.frameless)
@@ -37,16 +40,27 @@ pub fn create_window() -> Result<(Window, EventLoop<WebviewEvent>)> {
         .build(&event_loop)
         .unwrap();
 
-    Ok((window, event_loop))
-}
-
-pub fn create_webview(window: Window, proxy: EventProxy) -> Result<WebView> {
-    use wry::webview::WebViewBuilder;
     let webview = WebViewBuilder::new(window).unwrap();
     let webview = webview
         .with_initialization_script(&get_initialization_script())
-        .with_custom_protocol("nslauncher".to_string(), move |_, _| {
-            Ok((get_runtime(), "text/html".to_string()))
+        .with_custom_protocol("nslauncher".to_string(), move |request| {
+            let path = request
+                .uri()
+                .replace("nslauncher://", "")
+                .replacen("/", "", 1);
+
+            let content = get_runtime(&path);
+
+            let (data, meta) = if let Some(data) = content {
+                let mime_type = mime_guess::from_path(&path)
+                    .first_or_text_plain()
+                    .to_string();
+                (data, mime_type)
+            } else {
+                (get_runtime("index.html").unwrap(), "text/html".to_string())
+            };
+
+            ResponseBuilder::new().mimetype(&meta).body(data)
         })
         .with_url("nslauncher://")?
         .with_rpc_handler(move |_window, request| {
@@ -73,118 +87,115 @@ pub fn create_webview(window: Window, proxy: EventProxy) -> Result<WebView> {
                     )));
                 }
             }
-
             None
         })
         .build()?;
-    Ok(webview)
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            Event::UserEvent(event) => match event {
+                WebviewEvent::DispatchScript(s) => {
+                    webview.evaluate_script(&s).expect("Can't invoke js")
+                }
+                WebviewEvent::HideWindow => webview.window().set_visible(false),
+                WebviewEvent::Exit => {
+                    *control_flow = ControlFlow::Exit;
+                }
+                WebviewEvent::ShowWindow => webview.window().set_visible(true),
+                WebviewEvent::Emit(event, payload) => webview
+                    .evaluate_script(&format!(
+                        "window['{}']({{event: {}, payload: {}}})",
+                        "b03ebfab-8145-44ac-a4b6-d800ddfa3bba",
+                        serde_json::to_string(&event).expect("Can't serialize event name"),
+                        payload
+                    ))
+                    .expect("Can't emit event"),
+            },
+            _ => {
+                let _ = webview.resize();
+            }
+        }
+    });
 }
 
 #[cfg(feature = "bundle")]
-fn get_runtime() -> Vec<u8> {
-    include_crypt::include_crypt!(AES, "runtime/index.html").decrypt()
+fn get_runtime(path: &str) -> Option<Vec<u8>> {
+    let folder = include_crypt::include_dir!(AES, "runtime");
+    let file = folder.get(path);
+    file.map(EncryptedFile::decrypt)
 }
 
 #[cfg(not(feature = "bundle"))]
-fn get_runtime() -> Vec<u8> {
+fn get_runtime(path: &str) -> Option<Vec<u8>> {
     use std::fs;
-    fs::read("runtime/index.html").expect("Can't read lazy runtime file")
+    fs::canonicalize(&path).and_then(fs::read).ok()
 }
 
 #[cfg(windows)]
-pub fn download_webview2() {
+pub fn has_webview() -> bool {
+    use winreg::enums::*;
+    use winreg::RegKey;
+
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+
+    if hkcu
+        .open_subkey(
+            "Software\\Microsoft\\EdgeUpdate\\Clients\\{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}",
+        )
+        .is_ok()
+    {
+        return true;
+    }
+
+    let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
+
+    if cfg!(target_arch = "x86") {
+        if hklm
+            .open_subkey(
+                "SOFTWARE\\Microsoft\\EdgeUpdate\\Clients\\{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}",
+            )
+            .is_ok()
+        {
+            return true;
+        }
+    } else if cfg!(target_arch = "x86_64") && hklm.open_subkey(
+            "SOFTWARE\\WOW6432Node\\Microsoft\\EdgeUpdate\\Clients\\{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}",
+        ).is_ok() {
+        return true;
+    }
+
+    false
+}
+
+#[cfg(windows)]
+pub fn install_webview2() {
     use std::fs;
     use std::io::Write;
+
     let installer = minreq::get("https://go.microsoft.com/fwlink/p/?LinkId=2124703")
         .send()
         .unwrap();
     let body = installer.as_bytes().to_vec();
     let temp_dir = temp_dir::TempDir::new().expect("Can't create temdir");
     let install_path = temp_dir.child("install.exe");
-    let mut file = fs::OpenOptions::new()
-        .create(true)
-        .write(true)
-        .open(&install_path)
-        .unwrap();
-    file.write_all(&body).unwrap();
-    drop(file);
-    run_admin(&install_path, &["/silent", "/install"]).expect("Can't run installer");
-}
-
-#[cfg(target_os = "windows")]
-fn run_admin<FP: AsRef<Path>, AP: AsRef<Path>>(file: FP, args: &[AP]) -> Result<()> {
-    use std::ffi::OsStr;
-    use std::mem;
-    use std::os::windows::ffi::OsStrExt;
-
-    let mut params = String::new();
-    for arg in args.iter() {
-        let arg = arg.as_ref().to_string_lossy();
-        params.push(' ');
-        if arg.len() == 0 {
-            params.push_str("\"\"");
-        } else if arg.find(&[' ', '\t', '"'][..]).is_none() {
-            params.push_str(&arg);
-        } else {
-            params.push('"');
-            for c in arg.chars() {
-                match c {
-                    '\\' => params.push_str("\\\\"),
-                    '"' => params.push_str("\\\""),
-                    c => params.push(c),
-                }
-            }
-            params.push('"');
-        }
+    {
+        let mut file = fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(&install_path)
+            .unwrap();
+        file.write_all(&body).unwrap();
     }
-
-    let file = OsStr::new(file.as_ref())
-        .encode_wide()
-        .chain(Some(0))
-        .collect::<Vec<_>>();
-    let params = OsStr::new(&params)
-        .encode_wide()
-        .chain(Some(0))
-        .collect::<Vec<_>>();
-
-    unsafe {
-        use winapi::shared::minwindef::DWORD;
-        use winapi::um::processthreadsapi::GetExitCodeProcess;
-        use winapi::um::shellapi::{
-            ShellExecuteExW, SEE_MASK_NOASYNC, SEE_MASK_NOCLOSEPROCESS, SHELLEXECUTEINFOW,
-        };
-        use winapi::um::synchapi::WaitForSingleObject;
-        use winapi::um::winbase::INFINITE;
-        use winapi::um::winuser::SW_NORMAL;
-
-        let mut sei: SHELLEXECUTEINFOW = mem::zeroed();
-
-        sei.fMask = SEE_MASK_NOASYNC | SEE_MASK_NOCLOSEPROCESS;
-        sei.lpVerb = OsStr::new("runas")
-            .encode_wide()
-            .chain(Some(0))
-            .collect::<Vec<_>>()
-            .as_ptr();
-        sei.lpFile = file.as_ptr();
-        sei.lpParameters = params.as_ptr();
-        sei.nShow = SW_NORMAL;
-
-        let result = ShellExecuteExW(&mut sei);
-        if result == 0 || sei.hProcess.is_null() {
-            return Err(anyhow::anyhow!("Can't execute command"));
-        }
-
-        WaitForSingleObject(sei.hProcess, INFINITE);
-
-        let mut code: DWORD = mem::zeroed();
-        let result = GetExitCodeProcess(sei.hProcess, &mut code);
-
-        if result == 0 {
-            return Err(anyhow::anyhow!("Can't get proccess exit code"));
-        }
-    }
-
-    Ok(())
+    process::Command::new(install_path)
+        .args(&["/silent", "/install"])
+        .status()
+        .expect("Can't run installer");
 }
 
 fn get_initialization_script() -> String {
